18926432232a900c78cd968a033c462a63c539d9
worrawut <pworrawut@hotmail.com>
2014-10-12 20:53:41 +0700

    add controller for demo_app

---
+++ app/controllers/demo_app_controller.rb
@@ -0,0 +1,2 @@
+class DemoAppController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
-  root 'glacial-gorge-5715#application'
+  root 'demo_app#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
